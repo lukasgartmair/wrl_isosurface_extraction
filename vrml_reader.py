1dc4d62f621af060d1942b3264903655431b991e
lukasgartmair <lukas_gartmair@gmx.de>
2016-12-07 15:33:25 +0100

    successful extraction from ivas

--- vrml_reader.py
+++ vrml_reader.py
@@ -11,6 +11,8 @@ from mpl_toolkits.mplot3d import axes3d
 
 coords = np.genfromtxt('iso_coords_formatted.txt')
 
+indices = np.genfromtxt('iso_indices_formatted.txt',dtype=np.int64)
+
 fig = pl.figure()
 ax = fig.add_subplot(111, projection='3d')
 
@@ -18,9 +20,11 @@ ax.scatter(coords[:,0],coords[:,1],coords[:,2])
 
 f = open('isosurface.obj','w')
 for i in range(len(coords)):
-    f.write('v ' + str(coords[i,0]) + str(coords[i,1]) + str(coords[i,2]) + '\n') # python will convert \n to os.linesep
+    f.write('v ' + str(coords[i,0]) + ' ' + str(coords[i,1]) + ' ' +str(coords[i,2]) + '\n') # python will convert \n to os.linesep
 
-                       
+for i in range(len(indices)):
+    f.write('f ' + str(indices[i,0]+1) + ' ' + str(indices[i,1]+1) + ' '+ str(indices[i,2]+1) + '\n') # python will convert \n to os.linesep
+
                        
 f.close() # you can omit in most cases as the destructor will call it
     
